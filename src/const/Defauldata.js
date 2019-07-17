4e2093ca8358e6226aab53dd8a0ac5f6a29f2836
Danilo <danilorb20099@hotmail.com>
2019-07-17 16:21:42 -0600

    data por default agregada

---
+++ src/const/Defauldata.js
@@ -0,0 +1,108 @@
+const infotoshow = {
+  data: [
+    {
+      id: 0,
+      start: "Qu lili in ",
+      firtslink: "high force grid  day mission, the desing",
+      project: "new project",
+      iteration: "interation june",
+      img: "https://ca.slack-edge.com/TAWQZDNSG-UFXGGLJ04-820ce11447b1-48"
+    },
+    {
+      id: 1,
+      start: "pay litle in ",
+      firtslink: "high force grid  day mission, the desing",
+      project: "new project",
+      iteration: "interation june",
+      img: "https://ca.slack-edge.com/TAWQZDNSG-UBB6SN7CN-g7282f5d0193-48"
+    },
+    {
+      id: 2,
+      start: "lin don dong in ",
+      firtslink: "high force grid  day mission, the desing",
+      project: "new project",
+      iteration: "interation june",
+      img: "https://ca.slack-edge.com/TAWQZDNSG-UKU5EP26Q-df7f2d51eafd-48"
+    },
+    {
+      id: 3,
+      start: "start of the week",
+      firtslink: "high force grid  day mission, the desing",
+      project: "new project",
+      iteration: "interation june",
+      img: "https://ca.slack-edge.com/TAWQZDNSG-UKUJB03C5-edfcba307c99-48"
+    },
+    {
+      id: 4,
+      start: "lookin",
+      firtslink: "high force grid  day mission, the desing",
+      project: "new project",
+      iteration: "interation june",
+      img: "https://ca.slack-edge.com/TAWQZDNSG-UKWE35TGF-6e77ff963435-48"
+    }
+  ],
+  projects: [
+    {
+      id: 0,
+      name: "Ali",
+      description: "its a inner thing that they cant reach and cant reach",
+      footer: "science moving bricks",
+      date: "two days ago",
+      img:
+        "https://pbs.twimg.com/profile_images/877104939868278785/TF8f9XVP.jpg"
+    },
+    {
+      id: 1,
+      name: "Angular",
+      description:
+        "Hope its a good thing, maybe the best, good things will not die",
+      footer: "the whole grup is wu ya",
+      date: "two days ago",
+      img:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png"
+    },
+    {
+      id: 2,
+      name: "Ant Desing",
+      description:
+        "they are so many pubs in the town but she walked into my pub",
+      footer: "สวัสดี",
+      date: "two days ago",
+      img: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+    },
+    {
+      id: 3,
+      name: "Art Desing pro",
+      description:
+        "at that time, i only throught about what i wanted and i didnt want to",
+      footer: "programmer everyday",
+      date: "two days ago",
+      img: "https://avatars.githubusercontent.com/u/66291?v=3"
+    },
+    {
+      id: 4,
+      name: "Bootstrap",
+      description: "Winter is comming",
+      footer: "high force desing team",
+      date: "two days ago",
+      img:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Boostrap_logo.svg/250px-Boostrap_logo.svg.png"
+    },
+    {
+      id: 5,
+      name: "Reach",
+      description:
+        "life is like a box of chocolates you never know what you're gonna get",
+      footer: "lying you to learn computer",
+      date: "two days ago",
+      img: "https://cdn.worldvectorlogo.com/logos/react.svg"
+    }
+  ],
+  navalues: [
+    { id: 0, title: "Number of items", value: "56", subvalue: "" },
+    { id: 1, title: "Team Ranking", value: "8", subvalue: "/24" },
+    { id: 1, title: "Project Visit", value: "2,2334", subvalue: "" }
+  ]
+};
+
+export default infotoshow;
